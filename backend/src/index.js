da461d055e28306ede58bce032cfb599db60d061
Anish Mehta <mehtaanish111@gmail.com>
2024-12-06 23:35:30 +0530

    changed folder structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((e)=>{
+    console.log("Database connection failed",e);
+})
