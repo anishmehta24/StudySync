86b3e231d3d5753fdc4894cdfd2b31dc0697e7c9
Anish Mehta <mehtaanish111@gmail.com>
2024-12-12 01:02:29 +0530

    set up auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,25 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/authRoutes.js"
 
 const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+// API ENDPOINTS
+app.get("/",(req,res)=>
+    res.send("Api working")
+)
+app.use('/api/auth',authRouter);
 
 export {app}
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB = async()=>{
     try {
+        mongoose.connection.on('connected',()=>console.log("Database connected"));
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-       console.log("Database connected")
     } catch (error) {
         console.log("Database Connection error",error);
         process.exit(1);
