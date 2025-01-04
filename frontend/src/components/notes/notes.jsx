fb74c459294b7745ed5173a1eade908c95dea3d6
Anish Mehta <mehtaanish111@gmail.com>
2025-01-04 12:07:54 +0530

    notes upload done

--- frontend/src/components/notes/notes.jsx
+++ frontend/src/components/notes/notes.jsx
@@ -1,69 +1,66 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../Navbar';
-import { FaSearch, FaRegBookmark, FaRegThumbsUp, FaDownload, FaStreetView, FaShower, FaEye } from 'react-icons/fa';
+import { FaSearch, FaRegBookmark, FaRegThumbsUp, FaDownload, FaEye } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { AddCircle, AutoAwesome, ViewCarousel, ViewList, ViewModule } from '@mui/icons-material';
+import { AddCircle, AutoAwesome } from '@mui/icons-material';
 import axios from 'axios';
 import { AppContext } from '../../context/AppContext';
 
 const Notes = () => {
-  
   const { backendUrl } = useContext(AppContext);
-  const [notes,setNotes] = useState([]);
-
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     const fetchNotes = async () => {
       try {
         const { data } = await axios.get(`${backendUrl}/api/notes/getNotes`);
-        console.log(data?.data); 
-        setNotes(data.data || [])
+        console.log(data?.data);
+        setNotes(data.data || []);
       } catch (error) {
         console.error('Error fetching notes:', error);
       }
     };
 
     fetchNotes();
-  }, [backendUrl]); 
-
-  
+  }, [backendUrl]);
 
   const [selectedNote, setSelectedNote] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [filterTag, setFilterTag] = useState('');
 
   const filteredNotes = notes.filter((note) =>
-   
     note.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
     (filterTag ? note.tags.includes(filterTag) : true)
-);
+  );
 
   return (
     <div className="bg-gradient-to-r from-background to-secondary-light min-h-screen">
       <Navbar />
-      
-      <div className="container mx-auto py-16 px-10 ">
-        <div className=''>
-        <div className="text-center mb-8 shadow-md shadow-current rounded-xl">
-            <h2 className="text-4xl font-extrabold text-primary-dark  py-10 text-center tracking-wide">Share Your Knowledge</h2>
+
+      <div className="container mx-auto py-16 px-10">
+        <div className="">
+          <div className="text-center mb-8 shadow-md shadow-current rounded-xl">
+            <h2 className="text-4xl font-extrabold text-primary-dark py-10 text-center tracking-wide">
+              Share Your Knowledge
+            </h2>
             <p className="text-lg text-text-light max-w-2xl mx-auto mb-4">
               Upload your notes to help others learn and explore. Your contributions make this a valuable resource for everyone.
             </p>
-            <Link to='/notes/upload'> 
+            <Link to="/notes/upload">
               <button className="bg-primary-dark text-background px-6 py-3 mb-8 rounded-md hover:scale-105 transition duration-300 mt-4">
-                Upload Your Notes <AddCircle className='ml-2 mb-1' />
+                Upload Your Notes <AddCircle className="ml-2 mb-1" />
               </button>
             </Link>
-        </div>
+          </div>
         </div>
 
         <div className="flex justify-between items-center mt-4 mb-8">
           <h1 className="text-3xl font-bold text-primary-dark text-center tracking-normal">Featured Notes</h1>
-        <Link to='/ai-note-generation'> 
+          <Link to="/ai-note-generation">
             <button className="bg-primary-dark text-background px-4 py-2 rounded-md hover:scale-105 transition duration-100">
-            Generate Notes From AI <span><AutoAwesome className='mb-1'/></span>
+              Generate Notes From AI <span><AutoAwesome className="mb-1" /></span>
             </button>
-        </Link>
+          </Link>
         </div>
 
         {/* Search and Filter */}
@@ -90,30 +87,37 @@ const Notes = () => {
             <option value="AI">Artificial Intelligence</option>
             {/* Add more tags here */}
           </select>
-          
         </div>
 
         {/* Notes List */}
-        <div className="grid grid-cols-1 bg-gradient-to-r from-background to-secondary-light md:grid-cols-2  lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 bg-gradient-to-r from-background to-secondary-light md:grid-cols-2 lg:grid-cols-3 gap-8">
           {filteredNotes.map((note) => (
             <div
               key={note._id}
-              className=" border border-secondary-light p-4 rounded-lg shadow-lg shadow-current hover:shadow-xl hover:shadow-current transition duration-300 cursor-pointer hover:scale-105"
+              className="border border-secondary-light p-4 rounded-lg shadow-md shadow-current hover:shadow-lg hover:shadow-current transition duration-300 hover:scale-105 hover:cursor-pointer bg-gray-50 "
               onClick={() => setSelectedNote(note)}
             >
-              
-              <img src={note.thumbnail} alt={note.title} className="w-40 h-40 object-cover rounded-t-md mb-2" />
-              <h2 className="text-xl font-semibold text-primary-dark">{note.title}</h2>
-              <p className="text-text-light mt-2">by {note.author}</p>
-              <div className="mt-2">
-                  {note.tags.split(',').map((tag, index) => (
-                    <span
-                      key={index}
-                      className="text-sm bg-secondary-light text-primary px-2 py-1 rounded-full mr-2"
-                    >
-                      {tag.trim()}
-                    </span>
-                  ))}
+              <div className="relative mb-4">
+                <img
+                  src={note.thumbnail}
+                  alt={note.title}
+                  className="w-full h-40 object-cover rounded-t-md"
+                />
+                <div className="absolute top-2 left-2 bg-primary-dark text-background text-sm px-3 py-1 rounded-md">
+                  Uploaded by: {note.author}
+                </div>
+              </div>
+              <h2 className="text-xl font-bold text-primary-dark mb-2">{note.title}</h2>
+              <p className="text-sm text-text-light mb-4">{note.description?.slice(0, 100)}...</p>
+              <div className="flex flex-wrap">
+                {note.tags.split(',').map((tag, index) => (
+                  <span
+                    key={index}
+                    className="text-xs bg-secondary-light text-primary px-2 py-1 rounded-full mr-2 mb-2"
+                  >
+                    {tag.trim()}
+                  </span>
+                ))}
               </div>
             </div>
           ))}
@@ -121,33 +125,30 @@ const Notes = () => {
 
         {/* Note Detail Page */}
         {selectedNote && (
-          <div className="fixed inset-0 bg-background bg-opacity-90 flex justify-center items-center p-6">
+          <div className="fixed inset-0 bg-black bg-opacity-90 flex justify-center items-center p-6">
             <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
               <button onClick={() => setSelectedNote(null)} className="float-right text-primary-dark font-bold">
                 &times;
               </button>
-              <h2 className="text-2xl font-bold text-primary-dark mb-4">{selectedNote.title}</h2>
-              <p className="text-text mb-2">by {selectedNote.author}</p> 
+              <h2 className="text-2xl font-bold text-primary-dark mb-2">{selectedNote.title}</h2>
+              <p className="text-primary-dark mb-2 font-semibold">Uploaded by {selectedNote.author}</p>
               <div className="mb-4">
-                  {selectedNote.tags.split(',').map((tag, index) => (
-                    <span
-                      key={index}
-                      className="text-sm bg-secondary-light text-primary px-2 py-1 rounded-full mr-2"
-                    >
-                      {tag.trim()}
-                    </span>
-                  ))}
-            </div>
-              <p className="text-text mb-4">
-                {/* Sample content - Replace with actual note content */}
-               {selectedNote.description}
-              </p>
+                {selectedNote.tags.split(',').map((tag, index) => (
+                  <span
+                    key={index}
+                    className="text-sm bg-secondary-light text-primary px-2 py-1 rounded-full mr-2"
+                  >
+                    {tag.trim()}
+                  </span>
+                ))}
+              </div>
+              <p className="text-text mb-4">{selectedNote.description}</p>
               <div className="flex items-center space-x-4">
-                <button className="flex items-center bg-primary-dark text-background px-4 py-2 rounded-md hover:bg-primary transition duration-300"  onClick={() => window.open(selectedNote.file, '_blank')}>
+                <button
+                  className="flex items-center bg-primary-dark text-background px-4 py-2 rounded-md hover:bg-primary transition duration-300"
+                  onClick={() => window.open(selectedNote.file, '_blank')}
+                >
                   <FaEye className="mr-2" /> View Notes
-                </button>
-                <button className="flex items-center text-primary hover:text-primary-dark transition duration-300">
-                  <FaRegThumbsUp className="mr-1" /> Like
                 </button>
                 <button className="flex items-center text-primary hover:text-primary-dark transition duration-300">
                   <FaRegBookmark className="mr-1" /> Bookmark
@@ -162,28 +163,3 @@ const Notes = () => {
 };
 
 export default Notes;
-
-
-
-
-// const {
-//   GoogleGenerativeAI,
-//   HarmCategory,
-//   HarmBlockThreshold,
-// } = require("@google/generative-ai");
-
-// const apiKey = process.env.GEMINI_API_KEY;
-// const genAI = new GoogleGenerativeAI(apiKey);
-
-// const model = genAI.getGenerativeModel({
-//   model: "gemini-1.5-pro-002",
-// });
-
-// const generationConfig = {
-//   temperature: 1,
-//   topP: 0.95,
-//   topK: 40,
-//   maxOutputTokens: 8192,
-//   responseMimeType: "text/plain",
-// };
-
--- frontend/src/components/notes/uploadnotes.jsx
+++ frontend/src/components/notes/uploadnotes.jsx
@@ -1,19 +1,19 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../Navbar';
-import { Box, TextField, Typography, Button } from '@mui/material';
-import {  AddTask,  UploadFile,  UploadRounded,} from '@mui/icons-material';
+import { Box, TextField, Typography, Button, IconButton } from '@mui/material';
+import { AddTask, UploadFile, UploadRounded, Cancel } from '@mui/icons-material';
 import { AppContext } from '../../context/AppContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { FaEye } from 'react-icons/fa';
 
 const Uploadnotes = () => {
-  const {userData,backendUrl,setIsLoggedin,getUserData} = useContext(AppContext)
-  // console.log(userData);
+  const { userData, backendUrl } = useContext(AppContext);
   const username = userData?.name;
   const userId = userData?.id;
-  // console.log(userId);
   const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     title: '',
     description: '',
@@ -23,185 +23,214 @@ const Uploadnotes = () => {
   });
 
   const handleChange = (e) => {
-    const { name, value, files } = e.target;
+    const { name, files } = e.target;
     setFormData({
       ...formData,
-      [name]: files ? files[0] : value,
+      [name]: files ? files[0] : null,
     });
   };
 
-  const handleSubmit = async(e) => {
+  const removeFile = (field) => {
+    setFormData({
+      ...formData,
+      [field]: null,
+    });
+  };
 
+  const handleSubmit = async (e) => {
     e.preventDefault();
-   
-      axios.defaults.withCredentials = true
+    axios.defaults.withCredentials = true;
 
-       console.log(formData)
-          
-      const form = new FormData();
-      form.append('title', formData.title);
-      form.append('description', formData.description);
-      form.append('tags', formData.tags);
-      if (formData.image) form.append('image', formData.image);
-      form.append('document', formData.document);
-      form.append('uploadedBy', userId); 
+    const form = new FormData();
+    form.append('title', formData.title);
+    form.append('description', formData.description);
+    form.append('tags', formData.tags);
+    if (formData.image) form.append('image', formData.image);
+    form.append('document', formData.document);
+    form.append('uploadedBy', userId);
 
-      const { data } = await axios.post(backendUrl + '/api/notes/upload', form );
-      if(data.success) {
-        toast.success("Notes Uploaded Successfully")
-        navigate("/notes")
-      }
-      else{
-        toast.error(data.message)
-      }
+    const { data } = await axios.post(backendUrl + '/api/notes/upload', form);
+    if (data.success) {
+      toast.success('Notes Uploaded Successfully');
+      navigate('/notes');
+    } else {
+      toast.error(data.message);
+    }
   };
 
   return (
     <div>
-        <div><Navbar/></div>
-        
-    <div className="bg-gradient-to-r from-background to-secondary-light min-h-screen flex justify-center items-center p-6">
-      <div className="w-full max-w-md bg-gradient-to-r from-background to-secondary-light rounded-lg shadow-lg p-6">
-        <h2 className="text-2xl font-bold text-primary-dark mb-4 text-center tracking-normal">Upload Your Notes <UploadRounded/></h2>
-        <form onSubmit={handleSubmit}>
-      <Box mb={2}>
-        {/* <Typography variant="body1" color="textSecondary" component="label" htmlFor="title">
-          Title *
-        </Typography> */}
-        <TextField
-          variant="outlined"
-          label="Title"
-           placeholder="E.g., Quantum Physics, Basics of Calculus"
-          id="title"
-          name="title"
-          required
-          onChange={handleChange}
-          fullWidth
-          size="small"
-          margin="dense"
-        />
-      </Box>
+      <Navbar />
+      <div className="bg-gradient-to-r from-background to-secondary-light min-h-screen flex justify-center items-center p-6">
+        <div className="w-full max-w-md bg-gradient-to-r from-background to-secondary-light rounded-lg shadow-lg p-6">
+          <h2 className="text-2xl font-bold text-primary-dark mb-4 text-center tracking-normal">
+            Upload Your Notes <UploadRounded />
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <Box mb={2}>
+              <TextField
+                variant="outlined"
+                label="Title"
+                placeholder="E.g., Quantum Physics, Basics of Calculus"
+                id="title"
+                name="title"
+                required
+                onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                fullWidth
+                size="small"
+                margin="dense"
+              />
+            </Box>
 
-      <Box mb={2}>
-        {/* <Typography variant="body1" color="textSecondary" component="label" htmlFor="description">
-          Description *
-        </Typography> */}
-        <TextField
-          variant="outlined"
-          label="Description"
-          id="description"
-          placeholder="Short Brief About the Notes You are Uploading"
-          name="description"
-          required
-          onChange={handleChange}
-          multiline
-          rows={3}
-          fullWidth
-          size="small"
-          margin="dense"
-        />
-      </Box>
+            <Box mb={2}>
+              <TextField
+                variant="outlined"
+                label="Description"
+                id="description"
+                placeholder="Short Brief About the Notes You are Uploading"
+                name="description"
+                required
+                onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                multiline
+                rows={3}
+                fullWidth
+                size="small"
+                margin="dense"
+              />
+            </Box>
 
-      <Box mb={2}>
-        {/* <Typography variant="body1" color="textSecondary" component="label" htmlFor="tags">
-          Tags (comma-separated) *
-        </Typography> */}
-        <TextField
-          variant="outlined"
-             label="Tags (comma-separated)"
-           placeholder="E.g., Calculus, C++, Python, etc"
-          id="tags"
-          name="tags"
-          required
-          onChange={handleChange}
-          fullWidth
-          size="small"
-          margin="dense"
-        />
-      </Box>
+            <Box mb={2}>
+              <TextField
+                variant="outlined"
+                label="Tags (comma-separated)"
+                placeholder="E.g., Calculus, C++, Python, etc"
+                id="tags"
+                name="tags"
+                required
+                onChange={(e) => setFormData({ ...formData, tags: e.target.value })}
+                fullWidth
+                size="small"
+                margin="dense"
+              />
+            </Box>
 
-      <Box mb={2}>
-        <Typography variant="body1" color="textSecondary">
-          Upload Thumbnail (Optional)
-        </Typography>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          border="1px dashed"
-          borderRadius="8px"
-          py={1.5}
-          sx={{
-            cursor: 'pointer',
-            color: 'text.secondary',
-            '&:hover': { backgroundColor: 'action.hover' },
-          }}
-          onClick={() => document.getElementById('image').click()}
-        >
-          <UploadFile sx={{ mr: 1 }} />
-          <Typography>Choose a thumbnail image</Typography>
-        </Box>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          onChange={handleChange}
-          style={{ display: 'none' }}
-        />
-      </Box>
+            <Box mb={2}>
+              <Typography variant="body1" color="textSecondary">
+                Upload Thumbnail <span className='font-bold'> (Optional) </span>
+              </Typography>
+              {!formData.image ? (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  border="1px dashed"
+                  borderRadius="8px"
+                  py={1.5}
+                  sx={{
+                    cursor: 'pointer',
+                    color: 'text.secondary',
+                    '&:hover': { backgroundColor: 'action.hover' },
+                  }}
+                  onClick={() => document.getElementById('image').click()}
+                >
+                  <UploadFile sx={{ mr: 1 }} />
+                  <Typography>Choose a thumbnail image</Typography>
+                </Box>
+              ) : (
+                <Box display="flex" alignItems="center" justifyContent="space-between">
+                  <img
+                    src={URL.createObjectURL(formData.image)}
+                    alt="Uploaded Thumbnail"
+                    style={{ maxWidth: '80px', maxHeight: '80px', marginRight: '10px' }}
+                  />
+                  <IconButton onClick={() => removeFile('image')}>
+                    <Cancel color="error" />
+                  </IconButton>
+                </Box>
+              )}
+              <input
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+                onChange={handleChange}
+                style={{ display: 'none' }}
+              />
+            </Box>
 
-      <Box mb={2}>
-        <Typography variant="body1" color="textSecondary">
-          Upload Document (PDF or similar) *
-        </Typography>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          border="1px dashed"
-          borderRadius="8px"
-          py={1.5}
-          sx={{
-            cursor: 'pointer',
-            color: 'text.secondary',
-            '&:hover': { backgroundColor: 'action.hover' },
-          }}
-          onClick={() => document.getElementById('document').click()}
-        >
-          <UploadFile sx={{ mr: 1 }} />
-          <Typography>Choose a document file</Typography>
-        </Box>
-        <input
-          type="file"
-          id="document"
-          name="document"
-          required
-          accept=".pdf,.doc,.docx"
-          onChange={handleChange}
-          style={{ display: 'none' }}
-        />
-      </Box>
+            <Box mb={2}>
+              <Typography variant="body1" color="textSecondary">
+                Upload Notes (PDF or similar) *
+              </Typography>
+              {!formData.document ? (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  border="1px dashed"
+                  borderRadius="8px"
+                  py={1.5}
+                  sx={{
+                    cursor: 'pointer',
+                    color: 'text.secondary',
+                    '&:hover': { backgroundColor: 'action.hover' },
+                  }}
+                  onClick={() => document.getElementById('document').click()}
+                >
+                  <UploadFile sx={{ mr: 1 }} />
+                  <Typography>Choose a document file</Typography>
+                </Box>
+              ) : (
+                <Box display="flex" alignItems="center" justifyContent="space-between">
+                  <Typography display="flex"
+                    component="a"
+                    href={URL.createObjectURL(formData.document)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      textDecoration: 'none',
+                      color: '#007bff',
+                      fontWeight: 'bold',
+                      marginRight: '10px',
+                    }}
+                  >
+                   <FaEye className="mr-2 mt-1 ml-2" />  View Document
+                  </Typography>
+                  <IconButton onClick={() => removeFile('document')}>
+                    <Cancel color="error" />
+                  </IconButton>
+                </Box>
+              )}
+              <input
+                type="file"
+                id="document"
+                name="document"
+                required
+                accept=".pdf,.doc,.docx"
+                onChange={handleChange}
+                style={{ display: 'none' }}
+              />
+            </Box>
 
-      <Box mb={2}>
-        <Typography variant="body2" color="textSecondary">
-          Uploaded by: <span style={{ fontWeight: 'bold' }}>{username}</span>
-        </Typography>
-      </Box>
+            <Box mb={2}>
+              <Typography variant="body2" color="textSecondary">
+                Uploaded by: <span style={{ fontWeight: 'bold' }}>{username}</span> (You)
+              </Typography>
+            </Box>
 
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        fullWidth
-        sx={{ py: 1.5, fontSize: '1rem', fontWeight: 'medium' }}
-        className='hover:scale-105'
-      >
-        Upload <AddTask className='ml-1'/>
-      </Button>
-    </form>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ py: 1.5, fontSize: '1rem', fontWeight: 'medium' }}
+              className="hover:scale-105"
+            >
+              Upload <AddTask className="ml-1" />
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
