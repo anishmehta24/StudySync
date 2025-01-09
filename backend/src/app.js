049f1d50b7a7b4162e2daa1c9d17e3ecabd60ea8
Anish Mehta <mehtaanish111@gmail.com>
2025-01-09 19:55:12 +0530

    fix bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const app = express()
 
 
 const allowedOrigins = [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2,process.env.CORS_ORIGIN3];
+
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -21,7 +22,7 @@ app.use(cors({
   },
   credentials: true,
 }));
-
+app.options('*', cors());
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
